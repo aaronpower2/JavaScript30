b1f1576fd383b3b8e153ce16e7feabe82c528237
Aaron Power <aaronpower2@gmail.com>
2020-09-15 11:50:40 +0400

    modified:   app.js 	modified:   index-START.html 	modified:   style.css

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,27 @@
+/**
+ * Create an interactive drum kit using javascript and CSS to style the
+ * keys when they are pressed
+ */
+
+/* create the function to play the drum sound */
+function playSound(e){ // e = keyboard event
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    console.log(this);
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+
+window.addEventListener("keydown", playSound);
